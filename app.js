0ec7b564d17c5b90ed6d46232ecee4c19c119b37
Sujan Karmakar <sujankarmakar18122005@gmail.com>
2026-01-04 11:59:06 +0530

    JS Logic Part 2

--- app.js
+++ app.js
@@ -14,11 +14,15 @@ input.addEventListener("blur", () => {
 //Operators shouldn't follow each other and first char is digit
 let validInput = () => {
     let inputVal = input.value;
-    if(inputVal.length === 1 && operators.includes(inputVal)) {
-        alert("Start with a digit.");
+    if (inputVal.length === 1 && operators.includes(inputVal)) {
+        alert("Equation cannot start with a operator");
         input.value = inputVal.slice(0, inputVal.length - 1);
     }
-    if( operators.includes(inputVal[inputVal.length - 1]) &&  operators.includes(inputVal[inputVal.length - 2])) {
+    if (operators.includes(inputVal[inputVal.length - 3]) && inputVal[inputVal.length - 2] === '.' && operators.includes(inputVal[inputVal.length - 1])) {
+        alert("Operator cannot preceed and proceed decimal point simultaneously");
+        input.value = inputVal.slice(0, inputVal.length - 1);
+    }
+    if (operators.includes(inputVal[inputVal.length - 1]) && operators.includes(inputVal[inputVal.length - 2])) {
         alert("One operator cannot follow another.");
         input.value = inputVal.slice(0, inputVal.length - 1);
     }
@@ -32,9 +36,18 @@ input.addEventListener("input", function () {
     validInput();
 });
 
+//Equation should not end with operator
+input.addEventListener("change", function () {
+    let inputVal = input.value;
+    if (operators.includes(inputVal[inputVal.length - 1])) {
+        alert("Equation cannot end with a operator.");
+        input.value = inputVal.slice(0, inputVal.length - 1);
+    }
+});
+
 //Enter is equal to
 input.addEventListener("keydown", (event) => {
-    if(event.key === "Enter") {
+    if (event.key === "Enter") {
         let equal = document.querySelector("#equal");
         equal.click();
     } else if (event.key === '*') {
@@ -43,20 +56,48 @@ input.addEventListener("keydown", (event) => {
 });
 
 
-
+// HTML Buttons event Listener
 for (let btn of btns) {
     btn.addEventListener("click", () => {
         if (btn.innerText != '=') {
             input.value += `${btn.innerText}`;
             validInput();
         } else {
-            calculation();
+            let arr = numAndOperatorArr();
+            calculation(arr);
         }
     })
 }
 
-let calculation = () => {
-    let str = input.value;
-    
+//Store ops and nums in a array for infix calculation
+let numAndOperatorArr = () => {
+    let inputVal = input.value;
+    let nums = inputVal.split(/[+\-x\/]/);
+    let ops = [];
+
+    for (let i = 0; i < inputVal.length; i++) {
+        if (operators.includes(inputVal[i])) {
+            ops.push(inputVal[i]);
+        }
+    }
+
+    let numIdx = 0;
+    let opsIdx = 0;
+
+    let finalArr = [];
+
+    for (let i = 0; i < nums.length + ops.length; i++) {
+        if (i % 2) {
+            finalArr.push(ops[opsIdx++]);
+        } else {
+            finalArr.push(nums[numIdx++]);
+        }
+    }
+    return finalArr;
 }
 
+
+// Infix Calculation
+let calculation = (arr) => {
+    console.log(arr);
+}
